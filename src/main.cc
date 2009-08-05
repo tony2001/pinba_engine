cb8be7d79aa71e7335a14aec01e44f26b136b7db
Antony Dovgal <tony@daylessday.org>
2009-08-05 19:18:05 +0400

    fix FreeBSD build

--- src/main.cc
+++ src/main.cc
@@ -14,6 +14,8 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#include <sys/types.h>
+#include <sys/socket.h>
 #include "pinba.h"
 
 int pinba_collector_init(pinba_daemon_settings settings) /* {{{ */
