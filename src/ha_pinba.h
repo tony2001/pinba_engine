82e6894be5ce64fde71d6ea15cefbfc5e62eb0d6
Antony Dovgal <tony@daylessday.org>
2013-07-12 12:27:39 +0400

    drop final whitespaces

--- src/ha_pinba.h
+++ src/ha_pinba.h
@@ -82,7 +82,7 @@ class ha_pinba: public handler
 	inline int tags_fetch_row_by_name(unsigned char*, const unsigned char *name, uint name_len);
 
 	inline int tag_values_fetch_next(unsigned char *buf, size_t *index, size_t *position);
-	inline int tag_values_fetch_by_timer_id(unsigned char *buf); 
+	inline int tag_values_fetch_by_timer_id(unsigned char *buf);
 
 	inline int info_fetch_row(unsigned char *buf);
 	inline int report1_fetch_row(unsigned char *buf);
@@ -122,10 +122,10 @@ class ha_pinba: public handler
 	{
 	}
 
-	const char *table_type() const { 
-		return "PINBA"; 
+	const char *table_type() const {
+		return "PINBA";
 	}
-	
+
 	const char **bas_ext() const;
 	ulonglong table_flags() const
 	{
@@ -163,11 +163,11 @@ class ha_pinba: public handler
 	int index_read(unsigned char * buf, const unsigned char * key,	uint key_len, enum ha_rkey_function find_flag);
 	int index_next(unsigned char * buf);
 	int index_prev(unsigned char * buf);
-	/* 
-	 Even though the docs say index_first() is not required, 'SELECT * FROM <table> WHERE <index> > N' 
+	/*
+	 Even though the docs say index_first() is not required, 'SELECT * FROM <table> WHERE <index> > N'
 	 is not going to work without it. See mysql_ha_read() in sql_handler.cc, line ~548.
 	 */
-	int index_first(unsigned char * buf); 
+	int index_first(unsigned char * buf);
 
 	void position(const unsigned char *record);                   //required
 	int info(uint);                                               //required
@@ -178,6 +178,6 @@ class ha_pinba: public handler
 	THR_LOCK_DATA **store_lock(THD *thd, THR_LOCK_DATA **to, enum thr_lock_type lock_type);     //required
 };
 
-/* 
+/*
  * vim600: sw=4 ts=4 fdm=marker
  */
